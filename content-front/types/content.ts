14e77cf67a7e8d44bae79b77e96bac15eaaaf110
ayoubouakrim <ayoubnadorx1@gmail.com>
2026-04-22 23:34:37 +0100

    adding the editing page

--- content-front/types/content.ts
+++ content-front/types/content.ts
@@ -1,7 +1,15 @@
 export interface Content {
     id: number;
+    user_id: number;
     title: string;
-    description: string;
+    body: string;
+    meta_description: string;
+    platform: string;
+    status: ContentStatus;
+    content_type: ContentType;
     created_at: string;
     updated_at: string;
 }
+
+export type ContentStatus = "draft" | "scheduled" | "published" | "archived";
+export type ContentType = "blog_post" | "social_media_post" | "video_script" | "email_newsletter" | "product_description";
